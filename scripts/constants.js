c53adac66487c13ccb42532996dc9d10f12559d8
Suprim Shrestha <suprim.shresth@gmail.com>
2023-11-30 13:42:38 +0545

    Set constants values

---
+++ scripts/constants.js
@@ -0,0 +1,19 @@
+const BALL_COUNT = 50;
+const SPEED = 3;
+
+const VIEWPORT_WIDTH = 1000;
+const VIEWPORT_HEIGHT = 700;
+
+const MIN_BALL_RADIUS = 5;
+const MAX_BALL_RADIUS = 10;
+
+const COLOR_ARRAY = [
+  "#DFFF00",
+  "#FFBF00",
+  "#FF7F50",
+  "#DE3163",
+  "#9FE2BF",
+  "#40E0D0",
+  "#6495ED",
+  "#CCCCFF",
+];
